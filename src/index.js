3fa30fe4eb24d97315471d662f4ccf5e950990fb
Matheus Tanaka <matheustanakanog@gmail.com>
2022-11-04 21:17:48 -0300

    feat: add connection retry

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@ const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/c
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to Database")).catch((e) => console.log(e))
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to Database"))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    })
+}
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World</h1>")
