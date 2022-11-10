4497023e89dc43ded699311cd1aaaa2bbab26025
Matheus Tanaka <matheustanakanog@gmail.com>
2022-11-09 21:48:28 -0300

    feat: add login method

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,8 +3,9 @@ const bcrypt = require("bcryptjs")
 
 exports.signUp = async (req, res) => {
     const { username, password } = req.body
-    const hashpassword = await bcrypt.hash(password, 12)
+    
     try {
+        const hashpassword = await bcrypt.hash(password, 12)
         const newUser = await User.create({
             username,
             password: hashpassword
@@ -16,6 +17,38 @@ exports.signUp = async (req, res) => {
                 user: newUser
             }
         })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            status: "failed"
+        })
+    }
+}
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({username})
+
+        if(!user) {
+            return res.status(404).json({ status: "failed", message: "User not found"})
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if(isPasswordCorrect) {
+            res.status(200).json({
+                status: "success"
+            })
+        }
+        else {
+            res.status(404).json({
+                status: "failed",
+                message: "username or password are incorrect"
+            })
+        }
+
     } catch (error) {
         console.log(error)
         res.status(400).json({
