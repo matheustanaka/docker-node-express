7d150ffbaf298192e9a791025df5ba08280cc860
Matheus Tanaka <matheustanakanog@gmail.com>
2022-11-10 22:32:55 -0300

    feat: add redis env configuration

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,8 @@ module.exports = {
     MONGO_USER: process.env.MONGO_USER || "AzureDiamond",
     MONGO_PASSWORD: process.env.MONGO_PASSWORD || "hunter2",
     MONGO_PORT: process.env.MONGO_PORT || 27017,
-    MONGO_IP: process.env.MONGO_IP || "mongodb"
+    MONGO_IP: process.env.MONGO_IP || "mongodb",
+    REDIS_URL: process.env.REDIS_URL || "redis",
+    REDIS_PORT: process.env.REDIS_PORT || 6379,
+    SESSION_SECRET: process.env.SESSION_SECRET,
 }
